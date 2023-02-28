95b21f5eebbeca4a2a4c3b446285f5e1031f9cb9
aqeeluddin08 <122413610+aqeeluddin08@users.noreply.github.com>
2023-03-01 02:11:27 +0300

    Update players.component.ts

--- players.component.ts
+++ players.component.ts
@@ -3,4 +3,60 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-players',
-  templateUrl:
+  templateUrl: './players.component.html',
+  styleUrls: ['./players.component.css']
+})
+export class PlayersComponent implements OnInit {
+
+  players: any[] = [];
+  selectedPlayer: any = {};
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getPlayers();
+  }
+
+  getPlayers() {
+    this.http.get<any[]>('/api/players')
+      .subscribe(data => {
+        this.players = data;
+      });
+  }
+
+  addPlayer() {
+    this.selectedPlayer = {};
+  }
+
+  editPlayer(player: any) {
+    this.selectedPlayer = player;
+  }
+
+  savePlayer() {
+    if (this.selectedPlayer._id) {
+      this.http.put(`/api/players/${this.selectedPlayer._id}`, this.selectedPlayer)
+        .subscribe(() => {
+          this.getPlayers();
+          this.selectedPlayer = {};
+        });
+    } else {
+      this.http.post('/api/players', this.selectedPlayer)
+        .subscribe(() => {
+          this.getPlayers();
+          this.selectedPlayer = {};
+        });
+    }
+  }
+
+  deletePlayer(player: any) {
+    this.http.delete(`/api/players/${player._id}`)
+      .subscribe(() => {
+        this.getPlayers();
+      });
+  }
+
+  cancel() {
+    this.selectedPlayer = {};
+  }
+
+}
