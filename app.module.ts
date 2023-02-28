e1b10a631311bacf2a86412f2fdcc865c86483c7
aqeeluddin08 <122413610+aqeeluddin08@users.noreply.github.com>
2023-03-01 02:16:47 +0300

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+import { QueryComponent } from './query/query.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlayersComponent,
+    QueryComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
